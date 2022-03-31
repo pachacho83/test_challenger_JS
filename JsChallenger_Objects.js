e7938fac1ecb9e9adb09e6a7c03f2d6f25e58231
Pachacho83 <evalenzg@gmail.com>
2022-03-31 12:57:26 -0300

    Accessing object properties one

--- JsChallenger_Objects.js
+++ JsChallenger_Objects.js
@@ -1,6 +1,12 @@
 /*********************** javascript Objects *******************************/
 
-/*** Accessing object properties one () ***/
+/*** Accessing object properties one (Accediendo a las propiedades del objeto uno) ***/
+// Escribir una función que tome un objeto con dos propiedades como argumento
+// Debe devolver el valor de la propiedad con clave país
+
+const accessingProperties = (obj) => obj.country;
+console.log(accessingProperties({  continent: 'Asia',  country: 'Japan'}), accessingProperties({  country: 'Sweden',  continent: 'Europe'}));
+
 /***  ***/
 /***  ***/
 /***  ***/
