338b253f58ed5139a8ffa276a4f65aeb50d8b07e
Pachacho83 <evalenzg@gmail.com>
2022-04-05 12:03:52 -0400

    Return the number of days between two dates

--- jsChallenger_Dates.js
+++ jsChallenger_Dates.js
@@ -9,7 +9,17 @@ console.log(checkDatesEqual(new Date('2000/01/01 08:00:00'), new Date('2000/01/0
             checkDatesEqual(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 08:00:00')), 
             checkDatesEqual(new Date('2001/01/01 08:00:00'), new Date('2000/01/01 08:00:00')));
 
-/***  () ***/
+/*** Return the number of days between two dates (Devuelve el número de días entre dos fechas) ***/
+// Escribir una función que tome dos instancias de fecha como argumento
+// Debería devolver el número de días que hay entre esas fechas
+
+const returnNumberDaysBetweenDates = (a, b) => Math.abs(a-b)/(1000 * 3600 * 24);
+/*Solucion Autor
+  const dif = Math.abs(a - b);
+  return dif / 1000 / 60 / 60 / 24
+*/
+console.log(returnNumberDaysBetweenDates(new Date('2020-06-11'), new Date('2020-06-01')), returnNumberDaysBetweenDates(new Date('2000-01-01'), new Date('2020-06-01')));
+
 /***  () ***/
 /***  () ***/
 /***  () ***/
