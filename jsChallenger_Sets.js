ef2654eee0baa50a8d54e7b022e988be2f892fa7
Pachacho83 <evalenzg@gmail.com>
2022-04-06 16:42:16 -0400

    Check if value is present in Set

---
+++ jsChallenger_Sets.js
@@ -0,0 +1,13 @@
+/*** Check if value is present in Set (Compruebe si el valor está presente en Set) ***/
+// Escribir una función que tome un Set y un valor como argumentos
+// Comprobar si el valor está presente en el Conjunto
+
+const checkValueInSet = (set, val) => set.has(val);
+console.log(checkValueInSet(new Set([1, 2, 3]), 2), checkValueInSet(new Set([123]), 2), checkValueInSet(new Set(['1', '2', '3']), '2'), checkValueInSet(new Set('123'), '2'));
+
+/***  () ***/
+/***  () ***/
+/***  () ***/
+/***  () ***/
+/***  () ***/
+/***  () ***/
