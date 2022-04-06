e3aadf58a0db6eb0887d3b8b9720d13a2e64c7a9
Pachacho83 <evalenzg@gmail.com>
2022-04-06 18:36:10 -0400

    Get union of two sets

--- jsChallenger_Sets.js
+++ jsChallenger_Sets.js
@@ -13,7 +13,20 @@ console.log(checkValueInSet(new Set([1, 2, 3]), 2), checkValueInSet(new Set([123
 const convertSetToArray = (set) => [...set];
 console.log(convertSetToArray(new Set([1, 2, 3])), convertSetToArray(new Set([123])), convertSetToArray(new Set(['1', '2', '3'])), convertSetToArray(new Set('123')));
 
-/***  () ***/
+/*** Get union of two sets (Obtener unión de dos conjuntos.) ***/
+// Escribir una función que tome dos Conjuntos como argumentos
+// Crear la unión de los dos conjuntos
+// Devuelve el resultado
+// Consejo: trate de no cambiar a matrices, esto ralentizaría su código
+
+const getUnionSets = (a, b) => new Set([...a, ...b]);
+/*Solucion Autor
+  const result = new Set(a);
+  b.forEach((el) => result.add(el));
+  return result;
+*/
+console.log(getUnionSets(new Set('123'), new Set('234')), getUnionSets(new Set([1, 2, 3]), new Set([3, 4, 5])), getUnionSets(new Set([false, false, NaN]), new Set([true, true, NaN])));
+
 /***  () ***/
 /***  () ***/
 /***  () ***/
