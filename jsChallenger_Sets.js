f3cb9bb3227e97433e2bd98d087593e955ff9f1b
Pachacho83 <evalenzg@gmail.com>
2022-04-06 18:17:35 -0400

    Convert a Set to Array

--- jsChallenger_Sets.js
+++ jsChallenger_Sets.js
@@ -5,7 +5,14 @@
 const checkValueInSet = (set, val) => set.has(val);
 console.log(checkValueInSet(new Set([1, 2, 3]), 2), checkValueInSet(new Set([123]), 2), checkValueInSet(new Set(['1', '2', '3']), '2'), checkValueInSet(new Set('123'), '2'));
 
-/***  () ***/
+/*** Convert a Set to Array (Convertir un conjunto en matriz) ***/
+// Escribir una función que tome un Set como argumento
+// Convierte el conjunto en una matriz
+// Devuelve la matriz
+
+const convertSetToArray = (set) => [...set];
+console.log(convertSetToArray(new Set([1, 2, 3])), convertSetToArray(new Set([123])), convertSetToArray(new Set(['1', '2', '3'])), convertSetToArray(new Set('123')));
+
 /***  () ***/
 /***  () ***/
 /***  () ***/
