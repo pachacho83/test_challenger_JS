19277829ef1f9757ed5ded3c8a6ee2622cf3a75f
Pachacho83 <evalenzg@gmail.com>
2022-04-18 21:35:05 -0400

    comienzan con a

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+//cuantas Vocales hay en una cadena
+
 const vocales = ["a", "e", "i", "o", "u"];
 
 const saberVocales = (frase) => {
@@ -16,3 +18,21 @@ const text = "hola mundo";
 const totalVocales = saberVocales(text);
 
 console.log(`Numero de Vocales ${ totalVocales }`);
+
+//cuantas palabras empieza con a
+
+const palabras = ['andrea', 'olga', 'lalo', 'fernando', 'osvaldo', 'angela', 'melanie'];
+
+const comienzaConA = (arrayPalabra) => {
+
+  let contador = 0;
+
+  for (const item of arrayPalabra) {
+    if([...item][0].toLowerCase() === 'a')
+      contador++;
+  }
+
+  return contador;
+}
+
+console.log(`Numero de palabras que empiezan con A son:  ${ comienzaConA(palabras) }`);
